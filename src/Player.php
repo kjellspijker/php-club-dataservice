fa44bff441669a127e65f40da0886ea27fae403f
Kjell Spijker <5618282+kjellspijker@users.noreply.github.com>
2020-11-20 10:43:36 +0100

    Added players from the team-indeling

---
+++ src/Player.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PendoNL\ClubDataservice;
+
+/**
+ * Class Player
+ *
+ * @package PendoNL\ClubDataservice
+ */
+class Player extends AbstractItem
+{
+
+}
--- src/Team.php
+++ src/Team.php
@@ -4,8 +4,12 @@ namespace PendoNL\ClubDataservice;
 
 class Team extends AbstractItem
 {
+
     /** @var array $competitions */
     private $competitions = [];
+
+    /** @var array $players */
+    private $players = [];
 
     /**
      * All competitions where this team is active in.
@@ -14,16 +18,55 @@ class Team extends AbstractItem
      */
     public function getCompetitions()
     {
-        if (count($this->competitions) != 0) {
+        if (count($this->competitions) != 0)
+        {
             return $this->competitions;
         }
 
-        foreach($this->api->getClub()->getCompetitions() as $competition) {
-            if($competition->teamcode == $this->teamcode) {
+        foreach ($this->api->getClub()->getCompetitions() as $competition)
+        {
+            if ($competition->teamcode == $this->teamcode)
+            {
                 $this->competitions[] = $competition;
             }
         }
 
-        return  $this->competitions;
+        return $this->competitions;
+    }
+
+    /**
+     * All non-redacted players of the team
+     *
+     * @param array $arguments
+     *
+     * @return array
+     * @throws \PendoNL\ClubDataservice\Exception\InvalidResponseException
+     */
+    public function getTeamLayout($arguments = [])
+    {
+        $response = $this->api->request(
+            'team-indeling',
+            array_merge(
+                [
+                    'teamcode'       => $this->teamcode,
+                    'lokaleteamcode' => -1,
+                    'teampersoonrol' => 'ALLES',
+                    'toonlidfoto'    => 'NEE',
+                ],
+                $arguments
+            )
+        );
+
+        foreach ($response as $item)
+        {
+            $player = new Player($this->api, $item);
+
+            if ($player->naam !== 'Afgeschermd' && ! array_key_exists($player->relatiecode, $this->players))
+            {
+                $this->players[$player->relatiecode] = $player;
+            }
+        }
+
+        return $this->players;
     }
 }
